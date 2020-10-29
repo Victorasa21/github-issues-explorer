ee6a0e101cb55687aa91d69adc14438b18be9b8e
Luís Hansen and Túlio Assis <luis.hansen+tulio.assis@thoughtworks.com>
2020-10-29 18:52:37 -0300

    tests: added tests for the Dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Router } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories, Error } from './style';
 import logo from '../../assets/logo.svg';
@@ -74,21 +74,19 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map((repository) => (
-          <Link
-            key={repository.full_name}
-            to={`/repository/${repository.full_name}`}
-          >
-            <img
-              src={repository.owner.avatar_url}
-              alt={repository.owner.login}
-            />
-            <div>
-              <strong>{repository.full_name}</strong>
-              <p>{repository.description}</p>
-            </div>
-
-            <FiChevronRight size={30} />
-          </Link>
+          <span key={repository.full_name}>
+            <a href={`/repository/${repository.full_name}`}>
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+              <FiChevronRight size={30} />
+            </a>
+          </span>
         ))}
       </Repositories>
     </>
