b972afabe08451d21d98b66ed3b719c816c71bd5
Luís Hansen and Túlio Assis <luis.hansen+tulio.assis@thoughtworks.com>
2020-10-29 17:49:31 -0300

    fix: lint: fixed lint rules and files

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useRouteMatch, Link } from 'react-router-dom'
-import { Header, RepoProfile, RepoStats, Issues } from './style'
-import logo from '../../assets/logo.svg';
+import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import api from '../../services/api'
+import { Header, RepoProfile, RepoStats, Issues } from './style';
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+
 interface RepositoryParams {
   repository: string;
 }
@@ -16,7 +17,7 @@ interface Repository {
   open_issues_count: number;
   owner: {
     login: string;
-    avatar_url: string
+    avatar_url: string;
   };
 }
 
@@ -30,67 +31,70 @@ interface Issue {
 }
 
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>()
-  const [ repository, setRepository ] = useState<Repository | null>(null)
-  const [ issues, setIssues ] = useState<Issue[]>([])
+  const { params } = useRouteMatch<RepositoryParams>();
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
 
   useEffect(() => {
-    api.get(`/repos/${ params.repository }`).then((response) => (
-      setRepository(response.data)
-    )
-    )
-    api.get(`/repos/${ params.repository }/issues`).then((response) => (
-      setIssues(response.data)
-    )
-    )
-  }, [ params.repository ])
+    api
+      .get(`/repos/${params.repository}`)
+      .then((response) => setRepository(response.data));
+    api
+      .get(`/repos/${params.repository}/issues`)
+      .then((response) => setIssues(response.data));
+  }, [params.repository]);
 
   return (
     <>
       <Header>
-        <img src={ logo } alt="github" />
+        <img src={logo} alt="github" />
         <Link to="/">
           <FiChevronLeft />
-            voltar</Link>
+          voltar
+        </Link>
       </Header>
 
-      { repository && (
+      {repository && (
         <>
           <RepoProfile>
-            <img src={ repository.owner.avatar_url } alt={ repository.owner.login } />
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
             <div>
-              <strong>{ repository.full_name }</strong>
-              <p>{ repository.description }</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
           </RepoProfile>
           <RepoStats>
             <div>
-              <strong>{ repository.stargazers_count }</strong>
+              <strong>{repository.stargazers_count}</strong>
               <p>Stars</p>
             </div>
             <div>
-              <strong>{ repository.forks_count }</strong>
+              <strong>{repository.forks_count}</strong>
               <p>Forks</p>
             </div>
             <div>
-              <strong>{ repository.open_issues_count }</strong>
+              <strong>{repository.open_issues_count}</strong>
               <p>Issues abertas</p>
             </div>
-          </RepoStats></>) }
+          </RepoStats>
+        </>
+      )}
       <Issues>
-        { issues.map((issue) => (
-          <a key={ issue.id } href={ issue.html_url } >
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
             <div>
-              <strong>{ issue.title }</strong>
-              <p>{ issue.user.login }</p>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
             </div>
-            <FiChevronRight size={ 30 } />
+            <FiChevronRight size={30} />
           </a>
-        )) }
-
+        ))}
       </Issues>
     </>
-  )
-}
+  );
+};
 
-export default Repository
+export default Repository;
